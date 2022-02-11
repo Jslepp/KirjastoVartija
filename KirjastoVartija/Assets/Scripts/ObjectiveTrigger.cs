9ecced0c419e2287028f56b7f9c249125bdbdb2b
Jslepp <jslepp@utu.fi>
2022-02-11 17:24:13 +0200

    valot

--- KirjastoVartija/Assets/Scripts/ObjectiveTrigger.cs
+++ KirjastoVartija/Assets/Scripts/ObjectiveTrigger.cs
@@ -5,11 +5,13 @@ public class ObjectiveTrigger : MonoBehaviour
 {
     public Text teksti;
     public Animator anim;
+    public GameObject gameobject;
 
     //Upon collision with another GameObject, this GameObject will reverse direction
     private void OnTriggerEnter(Collider other)
     {
         anim.Play("TextFadeOut");
+        gameobject.SetActive(true);
     }
     private void OnTriggerExit(Collider other) 
     {
